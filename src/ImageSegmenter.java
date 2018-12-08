f7fb9d1d2956be18b139b28a14c9024b612463ed
Ryan Strauss <rystrauss@davidson.edu>
2018-12-08 11:16:52 -0500

    Initial commit

---
+++ src/ImageSegmenter.java
@@ -0,0 +1,13 @@
+import java.awt.Color;
+
+public class ImageSegmenter {
+
+    public static Color[][] segment(Color[][] rgbArray, double granularity) {
+        // COMPLETE ME!
+
+        // placeholder to ensure compilation -- replace this when you're done!
+        return rgbArray;
+    }
+
+}
+
